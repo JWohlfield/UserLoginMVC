9c84d3c9c98a7550cd2011613d5d11e373aa1f21
Jeffrey <JWohlfield@hotmail.com>
2021-02-24 20:17:54 -0500

    Intial commit

---
+++ UserLogin/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using UserLogin.Models;
+
+namespace UserLogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult UserLogin()
+        {
+            return View();
+        }
+
+        public IActionResult UserSummary()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UserLogin(User u)
+        {
+            if (ModelState.IsValid)
+            { //checking model state
+
+                return RedirectToAction("UserSummary",u);
+            }
+            return View(u);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ UserLogin/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserLogin.Models
+{
+    public class User
+    {
+        [Key]
+        [Required(ErrorMessage ="User Name is required!")]
+        public string UserId { get; set; }
+
+        [Required(ErrorMessage ="Password is required!")]
+        [StringLength(12, MinimumLength =5,ErrorMessage ="Password must be between 5-12 characters!")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage ="Age is required!")]
+        [Range(5,130,ErrorMessage ="Age must be between 5-130!")]
+        public int Age { get; set; }
+    }
+}
